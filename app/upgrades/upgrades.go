5cdd0dc73e236b3b3c2edd69ac272695b027b311
dev@kopi.money <dev@kopi.money>
2024-05-13 11:23:56 +0200

    Removed legacy upgrades

--- app/upgrades/upgrades.go
+++ app/upgrades/upgrades.go
@@ -1,19 +1,5 @@
 package upgrades
 
-import (
-	"github.com/kopi-money/kopi/app/upgrades/v0_3_3"
-	"github.com/kopi-money/kopi/app/upgrades/v0_3_4"
-)
-
 func UpgradeHandlers() Upgrades {
-	return Upgrades{
-		{
-			UpgradeName:          "v033",
-			CreateUpgradeHandler: v0_3_3.CreateUpgradeHandler,
-		},
-		{
-			UpgradeName:          "v034",
-			CreateUpgradeHandler: v0_3_4.CreateUpgradeHandler,
-		},
-	}
+	return Upgrades{}
 }
