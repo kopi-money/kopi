ea23b47ba91e22992a5993cea23b9b57fc95bcdd
dev@kopi.money <dev@kopi.money>
2024-12-17 14:40:57 +0100

    added upgrade handler for v0.6.5.2

--- app/upgrades/upgrades.go
+++ app/upgrades/upgrades.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kopi-money/kopi/app/upgrades/v0_6_3"
 	"github.com/kopi-money/kopi/app/upgrades/v0_6_4"
 	"github.com/kopi-money/kopi/app/upgrades/v0_6_5_1"
+        "github.com/kopi-money/kopi/app/upgrades/v0_6_5_2"
 )
 
 func UpgradeHandlers() Upgrades {
@@ -30,5 +31,9 @@ func UpgradeHandlers() Upgrades {
 			UpgradeName:          "v0_6_5_1",
 			CreateUpgradeHandler: v0_6_5_1.CreateUpgradeHandler,
 		},
+                {
+                        UpgradeName:          "v0_6_5_2",
+                        CreateUpgradeHandler: v0_6_5_2.CreateUpgradeHandler,
+                },
 	}
 }
