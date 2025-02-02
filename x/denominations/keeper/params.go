53376b41048edcfd5c4f56203456b590dd9dfc65
dev@kopi.money <dev@kopi.money>
2025-02-02 15:24:29 +0100

    sorting denoms by name

--- x/denominations/keeper/params.go
+++ x/denominations/keeper/params.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"sort"
 
 	"github.com/kopi-money/kopi/x/denominations/types"
 )
@@ -17,6 +18,14 @@ func (k Keeper) SetParams(ctx context.Context, params types.Params) error {
 	if err := params.Validate(); err != nil {
 		return err
 	}
+
+	sort.SliceStable(params.DexDenoms, func(i, j int) bool {
+		return params.DexDenoms[i].Name < params.DexDenoms[j].Name
+	})
+
+	sort.SliceStable(params.CollateralDenoms, func(i, j int) bool {
+		return params.CollateralDenoms[i].DexDenom < params.CollateralDenoms[j].DexDenom
+	})
 
 	k.params.Set(ctx, params)
 	return nil
