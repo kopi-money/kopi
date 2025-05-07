caee1669f785aecb916d8e0e3605f912adce083b
dev@kopi.money <dev@kopi.money>
2025-05-07 10:36:39 +0400

    removed special handling of low cAsset supply

--- x/mm/keeper/c_asset.go
+++ x/mm/keeper/c_asset.go
@@ -9,8 +9,6 @@ import (
 	"github.com/kopi-money/kopi/x/mm/types"
 )
 
-var minimumAmount = math.NewInt(1_000_000_000) // i.e. 1000
-
 // GetVaultAmount return the amount of funds held in the base denom of an CAsset. For example, when akUSD is the CAsset,
 // this functions return the amount of available kUSD
 func (k Keeper) GetVaultAmount(ctx context.Context, cAsset denomtypes.CAsset) math.Int {
@@ -23,40 +21,11 @@ func (k Keeper) getCAssetSupply(ctx context.Context, cAsset denomtypes.CAsset) m
 	return k.BankKeeper.GetSupply(ctx, cAsset.DexDenom).Amount
 }
 
-// CalculateNewCAssetAmount calculates how much new c-tokens have to be minted given how much value is being added to
-// the vault.
+// CalculateNewCAssetAmount calculates how much new cAsset tokens have to be minted given how much value is being added
+// to the vault.
 func (k Keeper) CalculateNewCAssetAmount(ctx context.Context, cAsset denomtypes.CAsset, addedAmount math.Int) (math.Int, error) {
 	cAssetSupply := k.getCAssetSupply(ctx, cAsset)
-	newTokens := math.ZeroInt()
-
-	amountBelowThreshold := minimumAmount.Sub(cAssetSupply)
-	if amountBelowThreshold.IsPositive() {
-		newTokens = math.MinInt(amountBelowThreshold, addedAmount)
-		addedAmount = addedAmount.Sub(newTokens)
-	}
-
-	if addedAmount.IsPositive() {
-		newTokensFromShare, err := k.CalculateNewCAssetAmountWithShare(ctx, cAsset, addedAmount)
-		if err != nil {
-			return math.Int{}, err
-		}
-
-		newTokens = newTokens.Add(newTokensFromShare)
-	}
-
-	return newTokens, nil
-}
-
-func (k Keeper) CalculateNewCAssetAmountWithShare(ctx context.Context, cAsset denomtypes.CAsset, addedAmount math.Int) (math.Int, error) {
-	cAssetSupply := k.getCAssetSupply(ctx, cAsset)
-	if cAssetSupply.LT(minimumAmount) {
-		return addedAmount, nil
-	}
-
-	loanSum := k.GetLoanSumWithDefault(ctx, cAsset.BaseDexDenom).LoanSum
-	vaultSize := k.GetVaultAmount(ctx, cAsset).ToLegacyDec()
-
-	cAssetValue := loanSum.Add(vaultSize)
+	cAssetValue := k.CalculateCAssetValue(ctx, cAsset)
 
 	newTotalValue := addedAmount.ToLegacyDec().Add(cAssetValue)
 	if !newTotalValue.IsPositive() {
